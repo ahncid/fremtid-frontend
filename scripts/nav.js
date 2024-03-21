e66a44c0ccd3e011d0388bef1f2a0eb4dbf19da0
ahncid <ahncid@gmail.com>
2024-03-21 15:37:58 +0100

    done

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,4 +1,3 @@
-// Først tilføjer vi en event listener til .ellipsis for at toggle active klassen ved klik
 document.querySelector(".ellipsis").addEventListener("click", function () {
   let navIcons = document.querySelector(".navigation .nav-icons");
   let ellipsis = document.querySelector(".ellipsis");
@@ -7,7 +6,6 @@ document.querySelector(".ellipsis").addEventListener("click", function () {
   ellipsis.classList.toggle("active");
 });
 
-// Derefter tilføjer vi en event listener til window for at fjerne active klassen ved scroll
 window.addEventListener("scroll", function () {
   let navIcons = document.querySelector(".navigation .nav-icons");
   let ellipsis = document.querySelector(".ellipsis");
--- scripts/navigate.js
+++ scripts/navigate.js
@@ -1,8 +1,7 @@
 document.querySelectorAll(".nav-icons li").forEach((item) => {
   item.addEventListener("click", function () {
     const url = this.getAttribute("data-href");
-    // Tjekker om elementet har en specifik attribut eller klasse
-    // der indikerer, at linket skal åbnes i en ny fane
+
     if (
       this.hasAttribute("target") &&
       this.getAttribute("target") === "_blank"
