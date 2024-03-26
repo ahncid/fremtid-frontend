01bf8a38a71e6af5442281ab8d18db400f795c08
ahncid <ahncid@gmail.com>
2024-03-26 12:25:35 +0100

    mappestruktur

--- scripts/loading.js
+++ scripts/loading.js
@@ -13,7 +13,7 @@ function simulateLoading() {
       const title = document.querySelector(".video-container");
       title.style.opacity = 0; // Set opacity to 0 for fade out effect
       setTimeout(() => {
-        window.location.href = "one-page.html";
+        window.location.href = "/html/one-page.html";
       }, 1500); // Vent yderligere før omdirigering
     }, 500); // Vent efter indlæsningen er "færdig" før fade out
   }, 3000); // Antag at indlæsning tager 3 sekunder
